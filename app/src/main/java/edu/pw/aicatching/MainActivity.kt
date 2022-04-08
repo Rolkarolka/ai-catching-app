4caeac09f25d62613f97f733fe174f88ab124ff3
Rolkarolka <romanowskarolina@gmail.com>
2022-04-08 21:14:52 +0200

    AC-8 | create activity

--- app/src/main/java/edu/pw/aicatching/MainActivity.kt
+++ app/src/main/java/edu/pw/aicatching/MainActivity.kt
@@ -2,10 +2,27 @@ package edu.pw.aicatching
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val showWardrobeButton = findViewById<Button>(R.id.showWardrobeButton)
+        val openCameraButton = findViewById<Button>(R.id.openCameraButton)
+
+        openCameraButton.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
+
+        showWardrobeButton.setOnClickListener {
+            val intent = Intent(this, WardrobeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
