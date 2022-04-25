780c10a8c923f481a4b1cfc27f7bb42f1803cd9f
Rolkarolka <romanowskarolina@gmail.com>
2022-04-25 23:38:02 +0200

    AC-11 | change toolbar to action bar

--- app/src/main/java/edu/pw/aicatching/MainActivity.kt
+++ app/src/main/java/edu/pw/aicatching/MainActivity.kt
@@ -1,6 +1,8 @@
 package edu.pw.aicatching
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 
@@ -13,6 +15,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_search -> {
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
 
     override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
 }
--- app/src/main/java/edu/pw/aicatching/WardrobeFragment.kt
+++ app/src/main/java/edu/pw/aicatching/WardrobeFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_navigation_bar.*
+
 
 class WardrobeFragment : Fragment() {
 
@@ -13,6 +16,7 @@ class WardrobeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_wardrobe, container, false)
     }
 }
