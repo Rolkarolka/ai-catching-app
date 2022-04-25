3cd9b2918d08756fc98ca5f9527cdd9919ce3d18
Rolkarolka <romanowskarolina@gmail.com>
2022-04-25 19:24:39 +0200

    AC-9 repair app

--- app/src/main/java/edu/pw/aicatching/LaunchFragment.kt
+++ app/src/main/java/edu/pw/aicatching/LaunchFragment.kt
@@ -1,17 +1,25 @@
 package edu.pw.aicatching
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
-class LaunchFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_launch, container, false)
+class LaunchFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(R.layout.fragment_launch, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler().postDelayed({
+            context?.let {
+                findNavController().navigate(R.id.action_launchFragment_to_homeFragment)
+            }
+        }, 2500)
     }
 }
