d6ceadbe09c84f889bfd8f7f18abf5aece03a4a5
Karolina Romanowska <62251997+Rolkarolka@users.noreply.github.com>
2022-05-08 22:58:44 +0200

    Ac 9 10 buttons on main page (#11)
    * AC-8 | append icons and buttons
    
    * AC-8 | append icons and buttons
    
    * AC-8 | png to svg
    
    * AC-8 | merge
    
    * AC-8 | create activity
    
    * AC-8 | append to manifest
    
    * AC-9 reformat
    
    * AC-9 to fragments
    
    * AC-9 repair app
    
    * AC-9 | append listener for camera
    
    * AC-9 | min sdk version
    
    * AC-9 | repair splash screen
    
    * AC-10
    
    * AC-10 append todo
    
    * AC-10 append todo
    
    * AC-9 after review
    
    * AC-11 top bar (#10)
    
    * AC-11 | append icons
    
    * AC-11 | format files
    
    * AC-11 | change toolbar to action bar
    
    * AC-11 | create bar
    
    * AC-11 hide and show action bar
    
    * AC-11 | clean up
    
    * AC-11 | new way for toolbar
    
    * AC-11 | change titles

--- app/src/main/java/edu/pw/aicatching/MainFragment.kt
+++ app/src/main/java/edu/pw/aicatching/MainFragment.kt
@@ -1,10 +1,10 @@
 package edu.pw.aicatching
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_main.*
 
