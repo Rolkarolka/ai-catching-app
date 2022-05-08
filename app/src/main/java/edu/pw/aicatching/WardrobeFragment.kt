83c59223282f2d24511cdb3325643a3c831d384d
Karolina Romanowska <62251997+Rolkarolka@users.noreply.github.com>
2022-05-08 22:48:10 +0200

    AC-11 top bar (#10)
    * AC-11 | append icons
    
    * AC-11 | format files
    
    * AC-11 | change toolbar to action bar
    
    * AC-11 | create bar
    
    * AC-11 hide and show action bar
    
    * AC-11 | clean up
    
    * AC-11 | new way for toolbar
    
    * AC-11 | change titles

--- app/src/main/java/edu/pw/aicatching/WardrobeFragment.kt
+++ app/src/main/java/edu/pw/aicatching/WardrobeFragment.kt
@@ -6,13 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
+
 class WardrobeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_wardrobe, container, false)
     }
+
+
 }
