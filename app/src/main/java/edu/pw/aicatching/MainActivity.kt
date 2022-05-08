11afc5e0a26aef8f81b3b2dea8b9ebdba5dd414e
Karolina Romanowska <62251997+Rolkarolka@users.noreply.github.com>
2022-05-08 22:47:16 +0200

    AC-9 AC-10 buttons on main page (#9)
    * AC-8 | append icons and buttons
    
    * AC-8 | append icons and buttons
    
    * AC-8 | png to svg
    
    * AC-8 | merge
    
    * AC-8 | create activity
    
    * AC-8 | append to manifest
    
    * AC-9 reformat
    
    * AC-9 to fragments
    
    * AC-9 repair app
    
    * AC-9 | append listener for camera
    
    * AC-9 | min sdk version
    
    * AC-9 | repair splash screen
    
    * AC-10
    
    * AC-10 append todo
    
    * AC-10 append todo
    
    * AC-9 after review

--- app/src/main/java/edu/pw/aicatching/MainActivity.kt
+++ app/src/main/java/edu/pw/aicatching/MainActivity.kt
@@ -1,11 +1,18 @@
 package edu.pw.aicatching
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_AICatching)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
 }
