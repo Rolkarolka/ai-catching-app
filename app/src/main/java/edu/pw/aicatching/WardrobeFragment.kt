cb00902d92aeb725ece3bb2c0b9f6c49cbd5d77a
Rolkarolka <romanowskarolina@gmail.com>
2022-05-16 18:14:35 +0200

    AC-12

--- app/src/main/java/edu/pw/aicatching/WardrobeFragment.kt
+++ app/src/main/java/edu/pw/aicatching/WardrobeFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.fragment_wardrobe.view.*
+import kotlinx.android.synthetic.main.wardrobe_gallery_item.view.*
 
 class WardrobeFragment : Fragment() {
     private lateinit var clothList: List<Cloth>
@@ -22,6 +25,12 @@ class WardrobeFragment : Fragment() {
             layoutManager = GridLayoutManager(mainActivity.activity, 2)
             adapter = WardrobeGalleryAdapter(clothList)
         }
+        view.wardrobe_gallery?.focusedChild?.setOnClickListener(View.OnClickListener() {
+            @Override
+            fun onClick(view: View) {
+                Navigation.findNavController(view).navigate(R.id.clothDescriptionFragment)
+            }
+        })
         return view
     }
 
@@ -47,6 +56,6 @@ class WardrobeFragment : Fragment() {
             "Bluza"
         )
 
-        return listOf(cloth_1, cloth_2, cloth_3, cloth_4)
+        return listOf(cloth_1, cloth_2, cloth_3, cloth_4, cloth_1, cloth_2, cloth_3, cloth_4)
     }
 }
