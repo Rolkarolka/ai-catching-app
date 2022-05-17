45dac664ec02ee8c8084b1886ab804b67b809699
Rolkarolka <romanowskarolina@gmail.com>
2022-05-17 08:14:13 +0200

    AC-13 | append permisions

--- app/src/main/java/edu/pw/aicatching/CameraFragment.kt
+++ app/src/main/java/edu/pw/aicatching/CameraFragment.kt
@@ -5,13 +5,96 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.concurrent.Executors
+import androidx.camera.core.*
+import androidx.camera.video.*
+import androidx.camera.video.VideoCapture
+import java.util.concurrent.ExecutorService
+import android.os.Build
+import edu.pw.aicatching.databinding.FragmentWardrobeBinding
+import kotlinx.android.synthetic.main.fragment_camera.view.*
+
+typealias LumaListener = (luma: Double) -> Unit
+
 
 class CameraFragment : Fragment() {
+    private lateinit var viewBinding: FragmentWardrobeBinding
+
+    private var imageCapture: ImageCapture? = null
+
+    private var videoCapture: VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
+
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+        val viewBinding = inflater.inflate(R.layout.fragment_camera, container, false)
+
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            this.activity?.let {
+                ActivityCompat.requestPermissions(
+                    it, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+            }
+        }
+        viewBinding.image_capture_button.setOnClickListener { takePhoto() }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        return viewBinding
+    }
+
+    private fun takePhoto() {}
+
+    private fun startCamera() {}
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        this.context?.let { it1 ->
+            ContextCompat.checkSelfPermission(
+                it1, it)
+        } == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this.context,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
     }
 }
