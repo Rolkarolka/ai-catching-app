e5bd9a0847b09c6f0193f796c76d61792adbfad7
Rolkarolka <romanowskarolina@gmail.com>
2022-05-17 16:09:08 +0200

    DEV | format

--- app/src/main/java/edu/pw/aicatching/LaunchActivity.kt
+++ app/src/main/java/edu/pw/aicatching/LaunchActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
 
-class LaunchActivity: AppCompatActivity() {
+class LaunchActivity : AppCompatActivity() {
     override fun onCreate(@Nullable savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val intent = Intent(this, MainActivity::class.java)
