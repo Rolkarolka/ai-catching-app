e047f76419c1b482aca846b152efdc18668480b6
Karolina Romanowska <62251997+Rolkarolka@users.noreply.github.com>
2022-05-17 16:18:10 +0200

    Dev (#15)
    * AC-4 | create new project (#1)
    
    * AC-5 | splash screen (#3)
    
    * AC-4 | create new project
    
    * AC-5 | append splash screen
    
    * AC-5 | larger icon
    
    * AC-8 | append icons and buttons (#6)
    
    * AC-8 | append icons and buttons
    
    * AC-8 | append icons and buttons
    
    * AC-8 | png to svg
    
    * AC-7 | create view (#5)
    
    * AC-6 | user top bar (#4)
    
    * AC-6 | fist version
    
    * AC-6 | UI rhombus with corners
    
    * AC-6 | after review
    
    * AC-6 | after review
    
    * AC-6 | png to svg
    
    * AC-6 | svg to xml
    
    * AC-6 | append style
    
    * AC-6 | merge
    
    * rename icon:
    
    * AC-9 AC-10 buttons on main page (#9)
    
    * AC-8 | append icons and buttons
    
    * AC-8 | append icons and buttons
    
    * AC-8 | png to svg
    
    * AC-8 | merge
    
    * AC-8 | create activity
    
    * AC-8 | append to manifest
    
    * AC-9 reformat
    
    * AC-9 to fragments
    
    * AC-9 repair app
    
    * AC-9 | append listener for camera
    
    * AC-9 | min sdk version
    
    * AC-9 | repair splash screen
    
    * AC-10
    
    * AC-10 append todo
    
    * AC-10 append todo
    
    * AC-9 after review
    
    * Ac 9 10 buttons on main page (#11)
    
    * AC-8 | append icons and buttons
    
    * AC-8 | append icons and buttons
    
    * AC-8 | png to svg
    
    * AC-8 | merge
    
    * AC-8 | create activity
    
    * AC-8 | append to manifest
    
    * AC-9 reformat
    
    * AC-9 to fragments
    
    * AC-9 repair app
    
    * AC-9 | append listener for camera
    
    * AC-9 | min sdk version
    
    * AC-9 | repair splash screen
    
    * AC-10
    
    * AC-10 append todo
    
    * AC-10 append todo
    
    * AC-9 after review
    
    * AC-11 top bar (#10)
    
    * AC-11 | append icons
    
    * AC-11 | format files
    
    * AC-11 | change toolbar to action bar
    
    * AC-11 | create bar
    
    * AC-11 hide and show action bar
    
    * AC-11 | clean up
    
    * AC-11 | new way for toolbar
    
    * AC-11 | change titles
    
    * AC-34 formatter (#13)
    
    * AC-34 append ktlint
    
    * AC-34
    
    * AC-34 | append github actions
    
    * AC-34 | append github actions
    
    * AC-34 | format
    
    * AC-34 | release
    
    * AC-34 | release
    
    * AC-34 | github actions
    
    * AC-34 | github actions
    
    * AC-34 | github actions
    
    * AC-34 | github actions
    
    * AC-34 | github actions
    
    * AC-34 | github actions
    
    * AC-34 | github actions
    
    * AC-34 | check github
    
    * AC-34 | format for subprojects
    
    * AC-34 format
    
    * DEV | format
    
    * AC-34 two checks
    
    * AC-34 on what
    
    * AC-34 typo

--- app/src/main/java/edu/pw/aicatching/MainActivity.kt
+++ app/src/main/java/edu/pw/aicatching/MainActivity.kt
@@ -1,11 +1,34 @@
 package edu.pw.aicatching
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_AICatching)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(),
+            fallbackOnNavigateUpListener = ::onSupportNavigateUp
+        )
+        main_toolbar.setupWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp() = navController.navigateUp(appBarConfiguration)
 }
