98d319787ad9c7ecbc5a2652bc51d8abfdd0b253
Karolina Romanowska <62251997+Rolkarolka@users.noreply.github.com>
2022-05-30 14:54:32 +0200

    AC-38 network and refactor (#32)
    * AC-38 xml refactor (#31)
    
    * AC-35 internet connection (#30)

--- app/src/main/java/edu/pw/aicatching/wardrobe/WardrobeFragment.kt
+++ app/src/main/java/edu/pw/aicatching/wardrobe/WardrobeFragment.kt
@@ -6,55 +6,46 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.GridLayoutManager
 import edu.pw.aicatching.R
-import edu.pw.aicatching.model.Cloth
+import edu.pw.aicatching.databinding.FragmentWardrobeBinding
+import edu.pw.aicatching.network.AICatchingApiService
+import edu.pw.aicatching.repositories.MainRepository
 import kotlinx.android.synthetic.main.fragment_wardrobe.view.*
 
 class WardrobeFragment : Fragment() {
-    private lateinit var clothList: List<Cloth>
+    private val service = AICatchingApiService.getInstance()
+    lateinit var viewModel: WardrobeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        clothList = clothInfo()
-        val view = inflater.inflate(R.layout.fragment_wardrobe, container, false)
-        val mainActivity = this
-        view.wardrobe_gallery.apply {
-            layoutManager = GridLayoutManager(mainActivity.activity, 2)
-            adapter = WardrobeGalleryAdapter(clothList) {
-                val bundle = bundleOf("clothCategory" to it.category, "clothImage" to it.image)
-                Navigation.findNavController(view).navigate(R.id.clothDescriptionFragment, bundle)
-            }
+    ): View {
+        val binding = FragmentWardrobeBinding.inflate(inflater)
+
+        val adapter = WardrobeGalleryAdapter {
+            val bundle = bundleOf("clothCategory" to it.id, "clothImage" to it.imgSrcUrl)
+            view?.let { it1 -> Navigation.findNavController(it1).navigate(R.id.clothDescriptionFragment, bundle) }
         }
+
+        viewModel = ViewModelProvider(this, WardrobeViewModelFactory(MainRepository(service))).get(WardrobeViewModel::class.java)
+        viewModel.wardrobeList.observe(
+            viewLifecycleOwner
+        ) {
+            adapter.setClothList(it)
+        }
+        viewModel.errorMessage.observe(viewLifecycleOwner) { }
+        viewModel.getWardrobe()
+
+        val mainActivity = this
+        val view = binding.root
+        view.wardrobeGallery.apply {
+            layoutManager = GridLayoutManager(mainActivity.activity, 2)
+        }
+        view.wardrobe_gallery.adapter = adapter
         return view
-    }
-
-    private fun clothInfo(): List<Cloth> {
-        val cloth_1 = Cloth(
-            1,
-            R.drawable.cloth,
-            "Blouse"
-        )
-        val cloth_2 = Cloth(
-            1,
-            R.drawable.cloth,
-            "Bluza"
-        )
-        val cloth_3 = Cloth(
-            1,
-            R.drawable.cloth,
-            "Blouse"
-        )
-        val cloth_4 = Cloth(
-            1,
-            R.drawable.cloth,
-            "Bluza"
-        )
-
-        return listOf(cloth_1, cloth_2, cloth_3, cloth_4, cloth_1, cloth_2, cloth_3, cloth_4)
     }
 }
