ff5d58b316d4eb2b30767b7b50145f21df77afca
Karolina Romanowska <62251997+Rolkarolka@users.noreply.github.com>
2022-11-20 00:42:54 +0100

    AC-43 show description of cloth (#39)
    * AC-43 Add list with attributes
    
    * AC-40 edit icon

--- app/src/main/java/edu/pw/aicatching/clothMatching/ClothDescriptionFragment.kt
+++ app/src/main/java/edu/pw/aicatching/clothMatching/ClothDescriptionFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import coil.load
+import edu.pw.aicatching.MainActivity
 import edu.pw.aicatching.R
+import kotlinx.android.synthetic.main.fragment_cloth_description.*
 import kotlinx.android.synthetic.main.item_cloth.view.*
 
 class ClothDescriptionFragment : Fragment() {
@@ -25,5 +28,11 @@ class ClothDescriptionFragment : Fragment() {
         view.clothCategory.text = clothCategory
         view.clothImage.load(imgUri)
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val attributesArray = arrayListOf("Type", "Color", "Length")
+        attributesListView.adapter = activity?.let { ArrayAdapter(it, R.layout.item_attribute, attributesArray) }
     }
 }
